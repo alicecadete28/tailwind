264c527833060b7ee2e883c8c3fb9fa716e12e22
Alice Cadete <abc3@cin.ufpe.br>
2024-10-24 21:10:36 -0300

    navbar

--- tailwind-treinamento-citi/tailwind.config.ts
+++ tailwind-treinamento-citi/tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  //indica os arquivos que estarao utilizando o tailwind css
+  // ** -> dentro de qualquer pasta
+  // * -> dentro de qualquer arquivo
+  // .{ts, tsx} -> que termina com .ts ou .tsx etc
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
