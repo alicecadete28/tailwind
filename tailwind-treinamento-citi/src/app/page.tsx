9fbec517ff01488062523e16e01a4f7bf50cb394
Alice Cadete <abc3@cin.ufpe.br>
2024-10-30 23:39:18 -0300

    navbar feita por mim

--- tailwind-treinamento-citi/src/app/page.tsx
+++ tailwind-treinamento-citi/src/app/page.tsx
@@ -1,85 +1,41 @@
 "use client";
 
-import { useState } from "react";
-
+// NAVBAR
 export default function Example() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-  console.log(isMenuOpen);
   return (
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="bg-purple-300 p-4">
-          <div className="flex items-center justify-between">
-            <div className="text-black text-2xl font-bold">ReactTailwind</div>
-
-            <div className="md:hidden">
-              <button className="text-black" onClick={toggleMenu}>
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={"2"}
-                  viewBox="0 0 24 24"
-                  className="w-6 h-6"
-                >
-                  <path d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-              </button>
+        <nav className="flex bg-purple-300 items-center justify-between">
+          {/* div da logo */}
+          <div className="flex items-center justify-start gap-2">
+            <div>
+              <img
+                src="logo-tailwind.svg"
+                className="w-10 md:w-10 lg:w-12 h-auto mx-auto"
+                alt="Logo"
+              />
             </div>
 
-            <ul className="hidden md:flex space-x-4">
-              <li>
-                <a href="#" className="text-purple-700">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-purple-700">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-purple-700">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-purple-700">
-                  Contact
-                </a>
-              </li>
-            </ul>
+            <div className="Logo-Color">React Tailwind</div>
           </div>
+          <div className="flex gap-2 mx-2">
+            <li>
+              <a href="#" className="text-purple-700">
+                Home
+              </a>
+            </li>
 
-          {/* Mobile Menu */}
-          {isMenuOpen ? (
-            <ul className="flex-col md:hidden">
-              <li className="py-2">
-                <a href="#" className="text-purple-700">
-                  Home
-                </a>
-              </li>
-              <li className="py-2">
-                <a href="#" className="text-purple-700">
-                  About
-                </a>
-              </li>
-              <li className="py-2">
-                <a href="#" className="text-purple-700">
-                  Services
-                </a>
-              </li>
-              <li className="py-2">
-                <a href="#" className="text-purple-700">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          ) : null}
+            <li>
+              <a href="#" className="text-purple-700">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-purple-700">
+                About Us
+              </a>
+            </li>
+          </div>
         </nav>
       </header>
     </div>
